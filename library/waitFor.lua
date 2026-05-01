a624f7a413e3bfb9a7bb880362a7ea72acf8439e
Studly <joelbreda@icloud.com>
2026-05-01 19:10:58 +0200

    refactor: add type annotations

--- library/waitFor.lua
+++ library/waitFor.lua
@@ -1,6 +1,6 @@
 local log = require(script.Parent.internalLogger)
 
-return function(parent, name, timeout)
+return function(parent: Instance, name: string, timeout: number?): Instance?
 	local result = parent:WaitForChild(name, timeout or 10)
 	if not result then
 		log:warn("waitFor: '" .. name .. "' not found in '" .. parent.Name .. "'")
