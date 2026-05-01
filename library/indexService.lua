2d91c1599a3c3eda0fd0c5b9af51eca0cfcc5e64
Studly <joelbreda@icloud.com>
2026-05-01 19:11:30 +0200

    refactor: add type annotations

--- library/indexService.lua
+++ library/indexService.lua
@@ -1,14 +1,12 @@
 local RunService = game:GetService("RunService")
 
-return function(name)
+return function(name: string): Instance?
 	if RunService:IsServer() then
 		return game:GetService(name)
 	end
-
 	local success, service = pcall(function()
 		return game:GetService(name)
 	end)
-
 	if success then return service end
 	return nil
 end
